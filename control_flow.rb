3c32dc5dc98c1a414ad7a3d3127aa0e41b81e5f7
Tyrone <tyrone.cloma0219@gmail.com>
2022-08-09 10:11:43 -0400

    solution

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,49 @@
 def admin_login(username, password)
-  # your code here
+  if username.downcase == 'admin' && password == '12345'
+    'Access granted'
+  else
+    'Access denied'
+  end
 end
+
+p 'Problem 1: ',admin_login('sudo','12345')
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature > 85
+    "It's too dang hot out there!"
+  elsif temperature < 65 && temperature > 40
+    "It's a little chilly out there!"
+  elsif temperature < 40
+    "It's brisk out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
+
+p 'Problem 2: ', hows_the_weather(100)
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else
+    num
+  end
 end
+
+puts 'Problem 3: ', fizzbuzz(30)
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+    when '+' then num1 + num2
+    when '-' then num1 - num2
+    when '*' then num1 * num2
+    when '/' then num1 / num2
+    else 'Invalid operation!'
+  end
 end
 
+puts 'Problem 4: ', calculator('a',10``,2)
